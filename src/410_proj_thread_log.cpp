7407af5d38cd391517c27fe6d4602931a9201b59
Josh Zutell <jmztx19@gmail.com>
2021-03-30 12:24:20 -0400

    initial implementation

--- src/410_proj_thread_log.cpp
+++ src/410_proj_thread_log.cpp
@@ -23,20 +23,40 @@ using namespace std;
  * @param info  to log
  * returns void
  */
-void fun(string info){
 
+bool bDoWork = true;
+
+void fun(string info){
+	Logger log1(LOG_FILE);
+	Logger log2(LOG_CONSOLE);
+
+	while (bDoWork) {
+		log1.Log(info);
+		log2.Log(info);
+		this_thread::sleep_for(chrono::milliseconds(10));
+	}
 }
 int main() {
 	
 	//TODO start as many threads as you have cores (see std::thread::hardware_concurrency())
 	//TODO save these threads in a vector
 
+	std::vector<thread> thread_vec;
+	int num_of_cores = std::thread::hardware_concurrency()/2;
+	for (int i = 0; i < num_of_cores; i++) {
+		thread_vec.push_back(thread(fun));
+	}
+
 	//TODO let threads run a bit (5 seconds)
 	this_thread::sleep_for(chrono::milliseconds(5000));
-	
+
 	//TODO ask them all to stop
+	bDoWork = false;
 	
 	//TODO wait for all threads to finish
+	for(auto& t : thread_vec) {
+			t.join();
+	}
 	
 	return 0;
 }
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -7,3 +7,40 @@
 #include <iostream>
 #include "../includes/Logger.h"
 using namespace std;
+
+/***
+ * creates a logger object, if lt=FILE then log to the file given by fn
+ * if lt=LOG_CONSOLE the send log info to console via cout
+ * @param lt either LOG_FILE or LOG_CONSOLE
+ * @param fn if above is LOG_FILE then this is where info logged to
+ *           otherwise it is not used
+ */
+Logger::Logger(LOG_TYPE lt, std::string fn) {
+	Logger::lt = lt;
+	if (lt == LOG_FILE) {
+		Logger::fn = fn;
+		Logger::fs.open(fn);
+	}
+}
+
+/***
+ * Information to log, either to a file or to console
+ * @param info to log
+ */
+void Logger::Log(std::string info) {
+	if (lt == LOG_FILE) {
+		const std::lock_guard<std::mutex> lock(mtx);
+		fs << info << endl;
+	}
+	else {
+		const std::lock_guard<std::mutex> lock(mtx);
+		cout << info << endl;
+	}
+}
+
+/***
+ * close any open streams
+ */
+Logger::~Logger() {
+	Logger::fs.close();
+}
